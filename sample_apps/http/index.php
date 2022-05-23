190743ab6bb9ce7cac9158ee56a5d86329dd9178
David Martini <88782651+davmartini@users.noreply.github.com>
2022-05-23 15:05:13 +0200

    Update index.php

--- sample_apps/http/index.php
+++ sample_apps/http/index.php
@@ -88,7 +88,7 @@
           <!-- Main jumbotron for a primary marketing message or call to action -->
           <div class="jumbotron" style="text-align:center">
             <h2>Bienvenue à cet événement Tech Data</h2>
-            <p>OpenShift est plus qu'une plateforme de container, c'est une plateforme cloud-native complète permettant le développement applicatif jusqu'à la mise en production d'environnements critiques</p>
+            <p>OpenShift est plus qu'une plateforme de containers, c'est une plateforme cloud-native complète permettant le développement applicatif jusqu'à la mise en production d'environnements critiques</p>
             <h3><?php echo "Container ID : ".gethostname(); ?></h3>
           </div>
           <div style="text-align:center">
