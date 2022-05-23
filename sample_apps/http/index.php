106975a8dfd34abf3257bf22ad3448cf26bb57c4
davmartini <88782651+davmartini@users.noreply.github.com>
2022-05-23 12:31:48 +0200

    test

--- sample_apps/http/index.php
+++ sample_apps/http/index.php
@@ -3,6 +3,18 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php
+ echo $_SERVER['PHP_SELF'];
+ echo "<br>";
+ echo $_SERVER['SERVER_NAME'];
+ echo "<br>";
+ echo $_SERVER['HTTP_HOST'];
+ echo "<br>";
+ // echo $_SERVER['HTTP_REFERER'];
+ echo "<br>";
+ echo $_SERVER['HTTP_USER_AGENT'];
+ echo "<br>";
+ echo $_SERVER['SCRIPT_NAME']; 
+ ?> 
  </body>
 </html>
