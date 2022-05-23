7f14553c604cccb366aefc250e6071ec3619d1a6
davmartini <88782651+davmartini@users.noreply.github.com>
2022-05-23 14:58:55 +0200

    test

--- sample_apps/http/index.php
+++ sample_apps/http/index.php
@@ -92,7 +92,7 @@
             <h3><?php echo "Container ID : ".gethostname(); ?></h3>
           </div>
           <div style="text-align:center">
-            <img src="images/techdata.png" width="280" height="125"/>
+            <img src="images/techdata.png" width="320" height="125"/>
           </div>
           <div style="background-color:blue;color:white;text-align:center">
             <p>Paris la Défense</p>
